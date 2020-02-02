12a029974de07331f28d1ea62bc62a40776961de
khinckley7 <35357105+khinckley7@users.noreply.github.com>
2020-02-02 15:33:12 -0500

    Update THE BOARD.py
    Added HTML tag stripping function and try/except block for cases with no data

--- THE BOARD.py
+++ THE BOARD.py
@@ -1,11 +1,29 @@
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
 import urllib
-from datetime import datetime
+from datetime import datetime, timedelta
 import urllib.request
 import csv
-import webbrowser
+# import webbrowser
+from html.parser import HTMLParser
+
+# Create HTML stripper
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+# HTML stripping function
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 # Open THE BOARD (2019) 
 file =  open('theboard2019.csv', 'r')
@@ -14,7 +32,7 @@ file =  open('theboard2019.csv', 'r')
 reader = csv.reader(file)
 next(reader) # Skips headers
 
-players = {} # Create dictionary of players to store id and position
+players = {} # Create dictionary of players to store id, position, and FV
 
 # Iterate through csv file
 for row in reader:
@@ -28,38 +46,55 @@ for row in reader:
 		pos = row[4]
 
 	# Add entry to dictionary
-	players[row[2].lower()] = {'id': row[20], 'position':pos}
+	players[row[2].lower()] = {'id': row[20], 'position':pos, 'FV':row[9]}
 
-today = datetime.today().strftime('%Y-%m-%d') # Today's date
+today = datetime.now() 
+start_date = (today - timedelta(days=365)).date() # 365 days prior
+today = today.strftime('%Y-%m-%d') # Today's date
 
-def milb_game_logs(player_name, player_id, position, start_date=0000-00-00, end_date=today):
+
+
+def milb_game_logs(player_name, player_id, position, start_date, end_date=today):
 
 	base = 'https://www.fangraphs.com/players/'
 
 	# Advanced stats table
 	url = base + str(player_name) + '/' + str(player_id) + '/game-log?position=' + str(position) + '&gds=' + str(start_date) + '&gde=' + str(end_date) + '&season=&type=-2'
-	# print(url)
-	# webbrowser.open(url)
 
-	page = urllib.request.urlopen(url)
-	soup = BeautifulSoup(page, 'lxml')
-	table = soup.find('table', class_='rgMasterTable') # Store table
+	try:
+		page = urllib.request.urlopen(url)
+		soup = BeautifulSoup(page, 'lxml')
+		table = soup.find('table', class_='rgMasterTable') # Store table
 
-	for col in table.select('th'):
-		matches = re.findall('(,d">)(\\S*)(<\\/a>)', col)
-		print(matches)
+		col_names = []
+		stats = []
 
-# for key, value in players.items():
+		for col in table.select('th'):
+			col = str(col)
+			col_name = strip_tags(col)
+			col_names.append(col_name)
+
+		for col in table.select('td'):
+			col = str(col)
+			stat = strip_tags(col)
+			stats.append(stat)
+
+
+		game_log = pd.DataFrame(list(zip(col_names, stats)), columns=[player_name,str(start_date)+'-'+str(end_date)])
+		print(game_log)
+
+	except:
+		pass
+
+
+for key, value in players.items():
 	
-# 	player_name = key
-# 	player_id = value['id']
-# 	position = value['position']
-	
-# 	milb_game_logs(player_name, player_id, position, 0000-00-00, today)
-
-milb_game_logs("aj-puk", "19343", "P", 0000-00-00, today)
+	player_name = key
+	player_id = value['id']
+	position = value['position']
+	FV = value['FV']
 
 
+	milb_game_logs(player_name, player_id, position, start_date, today)
 
-
-
+# milb_game_logs("aj-puk", "19343", "P", start_date, today)
